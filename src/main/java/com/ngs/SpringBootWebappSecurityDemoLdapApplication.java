915a1637bd78a83a92cb996ba75fa28fcb0ae6ac
ngsankar <ngsankar@gmail.com>
2020-01-15 13:49:12 -0500

    code base

---
+++ src/main/java/com/ngs/SpringBootWebappSecurityDemoLdapApplication.java
@@ -0,0 +1,13 @@
+package com.ngs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebappSecurityDemoLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebappSecurityDemoLdapApplication.class, args);
+	}
+
+}
